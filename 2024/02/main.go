43196d095295875a1827745694534835f78b47f0
gustavom2998 <42518882+gustavom2998@users.noreply.github.com>
2024-12-03 02:43:11 -0300

    add day 2 part 2

--- 2024/02/main.go
+++ 2024/02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -16,13 +17,15 @@ var input2 string
 func main() {
 	a := ProcessReports(input1)
 	if len(a) > 0 {
-		// Answer: 2
-		fmt.Printf("Input 1 - : %v\n", CountSafe(a))
+		// Answer: 2, 4
+        cs, cas := CountSafe(a)
+		fmt.Printf("Input 1 - : Safe: %v Almost Safe: %v\n", cs, cas)
 	}
 	b := ProcessReports(input2)
 	if len(b) > 0 {
-		// Answer: 2
-		fmt.Printf("Input 2 - : %v\n", CountSafe(b))
+		// Answer: 631, 665
+        cs, cas := CountSafe(b)
+		fmt.Printf("Input 2 - : Safe: %v Almost Safe: %v\n", cs, cas)
 	}
 }
 
@@ -52,11 +55,16 @@ func ProcessReports(reportFile string) [][]int {
 	return a
 }
 
-// CountSafe receives a slice of reports and counts the number of safe reports
-func CountSafe(a [][]int) (count int) {
+// CountSafe receives a slice of reports and counts the number of safe and almost safe reports
+func CountSafe(a [][]int) (countSafe, countAlmostSafe int) {
 	for _, r := range a {
-		if CheckSequence(r) {
-			count += 1
+		ok, _ := CheckSequence(r)
+		if ok {
+			countSafe += 1
+		}
+        ok  = CheckSequenceWithFailure(r)
+		if ok {
+			countAlmostSafe += 1
 		}
 	}
 	return
@@ -66,9 +74,9 @@ func CountSafe(a [][]int) (count int) {
 //
 // A sequence is safe if it constantly increases or decreases.
 // Each value cannot increase by less than 1 and more than 3 compared to the previous.
-func CheckSequence(a []int) (result bool) {
+func CheckSequence(a []int) (result bool, idx int) {
 	if len(a) <= 1 {
-		return true
+		return true, 0
 	}
 	result = true
 	lastDiff := 1
@@ -76,18 +84,47 @@ func CheckSequence(a []int) (result bool) {
 		currentDiff := a[i] - a[i-1]
 		// Check constantly increasing or decreasing
 		if (i > 1) && ((lastDiff > 0) != (currentDiff > 0)) {
-			return false
+			return false, i-1
 		}
 
 		// Check increase or decrease is greater than zero and less than four
 		if abs(currentDiff) < 1 || abs(currentDiff) > 3 {
-			return false
+			return false, i-1
 		}
 
 		lastDiff = currentDiff
 	}
 
-	return result
+	return result, 0
+}
+
+// CheckSequenceWithFailure is a modified version of CheckSequence that allows up to one failure
+func CheckSequenceWithFailure(a []int) (result bool) {
+	ok, idx := CheckSequence(a)
+	if ok {
+		return ok
+	}
+
+	if idx >= 1 {
+		left := slices.Clone(a)
+		tryLeft, _ := CheckSequence(slices.Delete(left, idx-1, idx))
+		if tryLeft {
+			return tryLeft
+		}
+	}
+
+	if idx < (len(a) - 1) {
+		right := slices.Clone(a)
+		tryRight, _ := CheckSequence(slices.Delete(right, idx+1, idx+2))
+		if tryRight {
+			return tryRight
+		}
+	}
+
+
+	item := slices.Clone(a)
+	tryItem, _ := CheckSequence(slices.Delete(item, idx, idx+1))
+	return tryItem
 }
 
 func abs(a int) int {
--- 2024/02/main_test.go
+++ 2024/02/main_test.go
@@ -7,7 +7,7 @@ import (
 	main "github.com/gustavom2998/advent-of-code/2024/02"
 )
 
-func TestExercise(t *testing.T) {
+func TestExerciseA(t *testing.T) {
 	type ReportCase struct {
 		Input []int
 		Want  bool
@@ -40,7 +40,60 @@ func TestExercise(t *testing.T) {
 	}
 
 	for i, report := range reports {
-		got := main.CheckSequence(report.Input)
+		got, _ := main.CheckSequence(report.Input)
+		want := report.Want
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("Report %v - CheckReport() mismatch (-want +got):\n%s", i, diff)
+		}
+	}
+}
+
+func TestExerciseB(t *testing.T) {
+	type ReportCase struct {
+		Input []int
+		Want  bool
+	}
+	reports := []ReportCase{
+		ReportCase{
+			Input: []int{7, 6, 4, 2, 1},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{1, 2, 7, 8, 9},
+			Want:  false,
+		},
+		ReportCase{
+			Input: []int{9, 7, 6, 2, 1},
+			Want:  false,
+		},
+		ReportCase{
+			Input: []int{1, 3, 2, 4, 5},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{8, 6, 4, 4, 1},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{1, 3, 6, 7, 9},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{6, 7, 9, 11, 13, 14, 18},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{1, 5, 6, 7},
+			Want:  true,
+		},
+		ReportCase{
+			Input: []int{81, 82, 81, 80, 79},
+			Want:  true,
+		},
+	}
+
+	for i, report := range reports {
+		got := main.CheckSequenceWithFailure(report.Input)
 		want := report.Want
 		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("Report %v - CheckReport() mismatch (-want +got):\n%s", i, diff)
