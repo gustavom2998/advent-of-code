74c07fa2bcc1103a6278c74643da5e0f68091936
gustavom2998 <42518882+gustavom2998@users.noreply.github.com>
2024-12-03 22:27:58 -0300

    add day 3 part 2

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -9,42 +9,125 @@ import (
 )
 
 //go:embed input2.txt
-var input1 string
+var input string
 
 func main() {
-	segments := strings.Split(input1, "\n")
-	fmt.Println(FindMultiples(segments))
+	fmt.Println(FindMultiples(input))
 }
 
-func FindMultiples(segments []string) int {
-	totalSum := 0
-	for _, s := range segments {
-		m, err := Multiples(s)
-		if err != nil {
-			fmt.Printf("unable to process segment: %v\n", err.Error())
-			return 0
-		}
-
-		sum := 0
-		for _, val := range m {
-			sum += val[0] * val[1]
-		}
-		totalSum += sum
+// FindMultiples can be used to get the multiples total sum and filtered sum
+func FindMultiples(s string) (totalSum int, filteredSum int) {
+	m, err := Multiples(s)
+	if err != nil {
+		fmt.Printf("unable to process segment: %v\n", err.Error())
+		return 0,0
 	}
-	return totalSum
+
+	sum := 0
+	for _, val := range m {
+		sum += val[0] * val[1]
+	}
+	totalSum += sum
+
+	m, err = FilteredMultiples(s)
+    fmt.Println(m)
+	if err != nil {
+		fmt.Printf("unable to process segment: %v\n", err.Error())
+		return 0,0
+	}
+
+	sum = 0
+	for _, val := range m {
+		sum += val[0] * val[1]
+	}
+	filteredSum += sum
+	
+	return
 }
 
+// State is used to represent the states used to identify multiples
 type State uint8
 
 const (
-	Start State = iota
-	MulWithPar
-	FirstOperand
-	SecondOperand
-	Comma
-	ClosePar
+	Start State = iota  // Start is the initial state
+	MulWithPar          // MulWithPar is the State when we've matched a mul command and need to find the operands
+	FirstOperand // FirstOperand is the state used to identify the digits for the first operand
+	SecondOperand // SecondOperand is the state used after identifying a comma after the first operand
 )
+// Filtered multiples returns the multiple pairs check for do(). 
+//
+// This could be simplified to use a regex but I wanted to have fun with a Finite State Machine.
+func FilteredMultiples(str string) ([][]int, error) {
+	state := Start
+	pairs := [][]int{}
+	s := strings.Split(str, "")
+	numbers1 := []string{}
+	numbers2 := []string{}
 
+	do := true
+	for i := 0; i < len(s); {
+		switch state {
+		case Start:
+			if i < (len(s)-4) && slices.Equal(s[i:i+4], []string{"m", "u", "l", "("}) && do {
+				state = MulWithPar
+				i += 4
+			} else if i < (len(s)-4) && slices.Equal(s[i:i+4], []string{"d", "o", "(", ")"}) {
+				do = true
+				i += 4
+			} else if i < (len(s)-7) && slices.Equal(s[i:i+7], []string{"d", "o", "n", "'", "t", "(", ")"}) {
+                do = false
+				i += 7
+			} else {
+				i++
+			}
+		case MulWithPar:
+			if isDigit(s[i]) {
+				state = FirstOperand
+			}
+		case FirstOperand:
+			if isDigit(s[i]) {
+				numbers1 = append(numbers1, s[i])
+				i++
+			} else if s[i] == "," && len(numbers1) > 0 {
+				state = SecondOperand
+				i++
+			} else {
+				numbers1 = []string{}
+				state = Start
+			}
+		case SecondOperand:
+			if isDigit(s[i]) {
+				numbers2 = append(numbers2, s[i])
+				i++
+			} else if s[i] == ")" && len(numbers2) > 0 {
+				first, err := strconv.Atoi(strings.Join(numbers1, ""))
+				if err != nil {
+					return nil, err
+				}
+				second, err := strconv.Atoi(strings.Join(numbers2, ""))
+				if err != nil {
+					return nil, err
+				}
+				pairs = append(pairs, []int{first, second})
+				numbers1 = []string{}
+				numbers2 = []string{}
+				i++
+				state = Start
+			} else {
+				numbers1 = []string{}
+				numbers2 = []string{}
+				state = Start
+			}
+		default:
+			return nil, fmt.Errorf("invalid state")
+		}
+	}
+	return pairs, nil
+}
+
+// Filtered multiples returns the multiple pairs. 
+//
+// This could be simplified to use a regex but I wanted to have fun with a Finite State Machine.
 func Multiples(str string) ([][]int, error) {
 	state := Start
 	pairs := [][]int{}
--- 2024/03/main_test.go
+++ 2024/03/main_test.go
@@ -3,27 +3,37 @@ package main_test
 import (
 	"testing"
 
-	main "github.com/gustavom2998/advent-of-code/2024/03"
 	"github.com/google/go-cmp/cmp"
+	main "github.com/gustavom2998/advent-of-code/2024/03"
 )
 
 func TestExerciseA(t *testing.T) {
 	type MemorySegmentCase struct {
-		Input []string
-		Want  int
+		Input                   []string
+		WantTotal, WantFiltered int
 	}
 	segments := []MemorySegmentCase{
 		MemorySegmentCase{
-			Input: []string{"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"},
-			Want:  161,
+			Input:        []string{"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"},
+			WantTotal:    161,
+			WantFiltered: 8,
 		},
+        MemorySegmentCase{
+			Input:        []string{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"},
+			WantTotal:    161,
+			WantFiltered: 48,
+		},
+
 	}
 
 	for i, segment := range segments {
-		got := main.FindMultiples(segment.Input)
-		want := segment.Want
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("Segment %v - findMultiples() mismatch (-want +got):\n%s", i, diff)
+		gotTotal, gotFiltered := main.FindMultiples(segment.Input)
+		wantTotal, wantFiltered := segment.WantTotal, segment.WantFiltered
+		if diff := cmp.Diff(wantTotal, gotTotal); diff != "" {
+			t.Errorf("Segment %v - findMultiples() total mismatch (-want +got):\n%s", i, diff)
+		}
+		if diff := cmp.Diff(wantFiltered, gotFiltered); diff != "" {
+			t.Errorf("Segment %v - findMultiples() filtered mismatch (-want +got):\n%s", i, diff)
 		}
 	}
 
